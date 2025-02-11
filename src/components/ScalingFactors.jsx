2f111da69ef23b2f8890058942bf8ac60b45f633
Jacob Marsh <jake.marsh@live.com.au>
2025-02-10 20:24:01 -0500

    Pre-moving videocontrols to bottom and refactoring

--- src/components/ScalingFactors.jsx
+++ src/components/ScalingFactors.jsx
@@ -38,14 +38,15 @@ const Controls = ({
   return (
   <div style={{ width: innerWidth, marginLeft: `${marginLeft}px`, textAlign: 'center' }}>
       
+      {/* Current stage text */}
+      <label className="w-44 mr-4">{currentStage}</label>
 
-    <div className="flex items-center ">
+    <div className="flex items-center mb-4">
 
       {/* Play/Pause toggle Button */}
       <button
           onClick={() => setIsPlaying(!isPlaying)}
           className="mr-2 px-4 py-2 rounded"
-          style={{ transform: 'scale(1.5)' }}
         >
           {isPlaying ? <FaPause /> : <FaPlay />}
         </button>
@@ -63,35 +64,24 @@ const Controls = ({
   valueLabelDisplay="auto"
   className="flex-grow mx-4"
   sx={{
-    color: '#888', // This sets the color of the track and thumb by default
+    color: 'grey', // This sets the color of the track and thumb by default
     '& .MuiSlider-thumb': {
-      backgroundColor: '#3e3c38',
-      boxShadow: 'none',  // Remove default box shadow
-      '&:hover': {
-        boxShadow: 'none',
-      },
-      '&.Mui-active': {
-        boxShadow: 'none',
-      },
-      '&.Mui-focusVisible': {
-        boxShadow: 'none',
-      },
+      backgroundColor: 'grey',
+      border: '2px solid grey',
     },
     '& .MuiSlider-track': {
-      backgroundColor: '#3e3c38',
+      backgroundColor: 'grey',
     },
     '& .MuiSlider-mark': {
-      backgroundColor: '#3e3c38',
+      backgroundColor: 'grey',
     },
     '& .MuiSlider-markLabel': {
-      color: '#3e3c38',
+      color: 'grey',
     }
   }}
 />
     </div>
 
-        {/* Current stage text */}
-        <label className="w-44">{currentStage}</label>
 
       {/* Scaling factors toggled as buttons */}
       <fieldset style={{ marginBottom: '1rem', border: 'none' }}>
